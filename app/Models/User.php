858285151e6561a00b6fbb41251d6d2566208386
suharyadi <suharyadi21@outlook.com>
2022-09-10 23:23:01 +0700

    - add spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Spatie\Permission\Traits\HasRoles;
+
+
 class User extends Authenticatable
 {
+    
     use HasFactory, Notifiable;
+
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
