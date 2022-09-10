3d97f4db25bc1fe4d54f67d1c4d91b9bb81c2d9b
suharyadi <suharyadi21@outlook.com>
2022-09-10 23:22:27 +0700

    - testing spatie

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class LanguageController extends Controller
-{
+{ 
+
+    public function __construct()
+    {
+        $this->middleware('permission:view posts', ['only' => ['index']]);
+        $this->middleware('permission:create posts', ['only' => ['create', 'store']]);
+        $this->middleware('permission:edit posts', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:delete posts', ['only' => ['destroy']]);
+        $this->middleware('permission:publish posts', ['only' => ['publish']]);
+        $this->middleware('permission:unpublish posts', ['only' => ['unpublish']]);
+    }
     //
     public function swap($locale){
       // available language in template array
@@ -15,5 +25,21 @@ class LanguageController extends Controller
           session()->put('locale',$locale);
       }
       return redirect()->back();
-  }
+    }
+
+    public function publish(int $id){
+        echo 'post berhasil dipublish';
+    }
+
+    public function unpublish(int $id){
+        echo 'post berhasil diunpublish';
+    }
+    public function edit(int $id){
+        echo 'edit berhasil edit';
+    }
+    public function destroy(int $id){
+        echo 'destroy berhasil destroy';
+    }
+
+
 }
