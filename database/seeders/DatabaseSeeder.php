285fc495a8b7a1967768c36b04cbfc3c666218a1
suharyadi <suharyadi21@outlook.com>
2022-09-10 23:23:54 +0700

    - add seeder user testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +20,56 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // reset cahced roles and permission
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'view posts']);
+        Permission::create(['name' => 'create posts']);
+        Permission::create(['name' => 'edit posts']);
+        Permission::create(['name' => 'delete posts']);
+        Permission::create(['name' => 'publish posts']);
+        Permission::create(['name' => 'unpublish posts']);
+
+        //create roles and assign existing permissions
+        $writerRole = Role::create(['name' => 'writer']);
+        $writerRole->givePermissionTo('view posts');
+        $writerRole->givePermissionTo('create posts');
+        $writerRole->givePermissionTo('edit posts');
+        $writerRole->givePermissionTo('delete posts');
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo('view posts');
+        $adminRole->givePermissionTo('create posts');
+        $adminRole->givePermissionTo('edit posts');
+        $adminRole->givePermissionTo('delete posts');
+        $adminRole->givePermissionTo('publish posts');
+        $adminRole->givePermissionTo('unpublish posts');
+
+        $superadminRole = Role::create(['name' => 'super-admin']);
+        // gets all permissions via Gate::before rule
+
+        // create demo users
+        $user = User::factory()->create([
+            'name' => 'Example user',
+            'email' => 'writer@qadrlabs.com',
+            'password' => bcrypt('12345678')
+        ]);
+        $user->assignRole($writerRole);
+
+        $user = User::factory()->create([
+            'name' => 'Example admin user',
+            'email' => 'admin@qadrlabs.com',
+            'password' => bcrypt('12345678')
+        ]);
+        $user->assignRole($adminRole);
+
+        $user = User::factory()->create([
+            'name' => 'Example superadmin user',
+            'email' => 'superadmin@qadrlabs.com',
+            'password' => bcrypt('12345678')
+        ]);
+        $user->assignRole($superadminRole);
     }
 }
