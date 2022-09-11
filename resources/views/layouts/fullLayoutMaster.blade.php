2df27bc0900849799191b8aedb82c612e7116ad1
suharyadi <suharyadi21@outlook.com>
2022-09-11 21:11:13 +0700

    - update title

--- resources/views/layouts/fullLayoutMaster.blade.php
+++ resources/views/layouts/fullLayoutMaster.blade.php
@@ -28,7 +28,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title') - Frest - Bootstrap HTML admin template</title>
+    <title>@yield('title') - Osmaru</title>
     <link rel="apple-touch-icon" href="{{asset('images/ico/apple-icon-120.png')}}">
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/ico/favicon.ico')}}">
 
