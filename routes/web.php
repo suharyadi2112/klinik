3e32fe6a8a09a4db9bc7f019f0a994d60332d45c
suharyadi <suharyadi21@outlook.com>
2022-09-11 21:12:22 +0700

    - update route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StarterKitController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\Auth\UserController;
+
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ManageRoles\ManageRoles;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,21 +19,35 @@ use App\Http\Controllers\Auth\UserController;
 |
 */
 
-// dashboard Routes
-Route::get('/', [StarterKitController::class, 'index'])->name('dashboard');
+/*
+|--------------------------------------------------------------------------
+| route template bawaan
+|--------------------------------------------------------------------------
+*/
 Route::get('/sk-layout-1-column', [StarterKitController::class, 'column_1Sk'])->name('1-column');
 Route::get('/sk-layout-2-columns', [StarterKitController::class, 'columns_2Sk'])->name('2-columns');
 Route::get('/fixed-navbar', [StarterKitController::class, 'fix_navbar'])->name('fixed-navbar');
 Route::get('/sk-layout-fixed', [StarterKitController::class, 'fix_layout'])->name('fixed-layout');
 Route::get('/sk-layout-static', [StarterKitController::class, 'static_layout'])->name('static-layout');
+/*
+|--------------------------------------------------------------------------
+*/
+
+
+// dashboard Routes
+
+Route::get('/', [StarterKitController::class, 'index'])->name('dashboard')->middleware('auth');
+
+// users Routes dengan spatie role and permission
+Route::group(['middleware' => ['role:super-admin|admin']], function () {//akses untuk super-admin dan admin
+	Route::get('/users/roles', [ManageRoles::class, 'ShowRolesUsers'])->name('ShowRolesUsers');
+	Route::post('/users/store/roles', [ManageRoles::class, 'StoreRoles'])->name('StoreRoles');
+	Route::delete('/users/del/roles/{id}', [ManageRoles::class, 'DelRoles'])->name('DelRoles');
+	Route::post('/users/put/roles/{id}', [ManageRoles::class, 'PutRoles'])->name('PutRoles');
+});
 
 // locale Route
 Route::get('lang/{locale}', [LanguageController::class, 'swap'])->name('language');
 
-//tes permission
-Route::get('publish/{id}', [LanguageController::class, 'publish'])->name('post.publish');
-Route::get('unpublish/{id}', [LanguageController::class, 'unpublish'])->name('post.unpublish');
-Route::get('edit/{id}', [LanguageController::class, 'edit'])->name('post.edit');
-Route::get('destroy/{id}', [LanguageController::class, 'destroy'])->name('post.destroy');
-
+//route global bawaan laravel
 Auth::routes(['verify' => true]);
