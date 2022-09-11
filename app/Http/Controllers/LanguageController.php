9feae5cc1cba47d0725fb9aa9f83f7119937577e
suharyadi <suharyadi21@outlook.com>
2022-09-11 21:08:33 +0700

    - del testing permission

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -6,17 +6,6 @@ use Illuminate\Http\Request;
 
 class LanguageController extends Controller
 { 
-
-    public function __construct()
-    {
-        $this->middleware('permission:view posts', ['only' => ['index']]);
-        $this->middleware('permission:create posts', ['only' => ['create', 'store']]);
-        $this->middleware('permission:edit posts', ['only' => ['edit', 'update']]);
-        $this->middleware('permission:delete posts', ['only' => ['destroy']]);
-        $this->middleware('permission:publish posts', ['only' => ['publish']]);
-        $this->middleware('permission:unpublish posts', ['only' => ['unpublish']]);
-    }
-    //
     public function swap($locale){
       // available language in template array
       $availLocale=['en'=>'en', 'fr'=>'fr','de'=>'de','pt'=>'pt'];
@@ -26,20 +15,5 @@ class LanguageController extends Controller
       }
       return redirect()->back();
     }
-
-    public function publish(int $id){
-        echo 'post berhasil dipublish';
-    }
-
-    public function unpublish(int $id){
-        echo 'post berhasil diunpublish';
-    }
-    public function edit(int $id){
-        echo 'edit berhasil edit';
-    }
-    public function destroy(int $id){
-        echo 'destroy berhasil destroy';
-    }
-
 
 }
