cdbc47f1cfb96010a6361f1dfc77699c3a9da18a
suharyadi <suharyadi21@outlook.com>
2022-09-15 00:52:38 +0700

    - log model dan migration

---
+++ database/migrations/2022_09_14_235803_create_log_activity_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogActivityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->string('url');
+            $table->string('method');
+            $table->string('ip');
+            $table->string('agent')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('log_activities');
+    }
+}
