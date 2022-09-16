f9e5d5ee5a50a7b380766c4173937678edb14a37
suharyadi <suharyadi21@outlook.com>
2022-09-16 16:40:21 +0700

    - update permission role

--- app/Http/Controllers/Auth/ManageRoles/ManagePermissions.php
+++ app/Http/Controllers/Auth/ManageRoles/ManagePermissions.php
@@ -13,6 +13,8 @@ use Yajra\Datatables\Datatables;
 
 use App\Models\User;
 use Spatie\Permission\models\Role;
+use Spatie\Permission\PermissionRegistrar;
+use App\Helpers\Helper as HelperLog;
 
 use DB;
 
@@ -30,21 +32,82 @@ class ManagePermissions extends Controller
 
      
     }
-    public function GetPermission(Role $role, Request $request){
 
-        $users = User::role('writer')->get();
+    //update permission
+    public function UpdatePermission(Request $request) {   
+        // reset cache permission
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        $res = DB::table('role_has_permissions')
-        ->select(   'role_has_permissions.*',
-                    'roles.name as name_roles', 'roles.guard_name as guard_name_roles','roles.id as id_roles',
-                    'permissions.name as name_permission','permissions.guard_name as guard_name_permission', 'permissions.id as id_permission')
-        ->join('roles', 'role_has_permissions.role_id','=','roles.id')
-        ->join('permissions', 'role_has_permissions.permission_id','=','permissions.id')
-        ->where('roles.name', '=', 'admin')
-        ->get();
-        // $users = user::with('permissions')->get();
+        $validator = Validator::make($request->all(), [
+            'permission_id' => 'required',
+            'roleid' => 'required'
+        ]);
 
-        dd($res);
+        if ($validator->fails()) {
+          return response()->json(['code' => '1', 'fail' => $validator->messages()->first()], 200);
+        }else{
+                $deleted = DB::table('role_has_permissions')->where('role_id', '=', $request->roleid)->delete();
+                // if ($deleted) {
+                    for ($i=0; $i < count($request->permission_id); $i++) { 
+                        $Insert[] =   [
+                                    'permission_id' => $request->permission_id[$i],
+                                    'role_id' => $request->roleid,
+                                    ];
+                    }
+                    $resInsert = DB::table('role_has_permissions')->insert($Insert);
+                    //param pertama subject dan kedua data request
+                    HelperLog::addToLog('Update permissions in role', json_encode($request->all())); 
+                    return response()->json(['code' => '2'], 200);
+                // }
+            }
+    
+    }
+
+    // show modal permissionf
+    public function ShowModalPermission(Request $request)
+    {   
+
+        HelperLog::addToLog('Show modal permission', json_encode($request->rolesid));
+        return response()->json(['code' => '2' , 'modalData' =>  $this->GetModal($request->rolesid)]);
 
     }
+
+    protected function GetModal($rolesid){
+
+        $modal = '';
+        $modal .= '<div class="modal-body" >
+                    <div class="form-group">
+
+                <table class="table table-sm table-striped">
+                    <tbody>';
+
+        $GroupPermission = DB::table("permissions")->select('group')->groupBy('group')->orderBy('group','DESC')->get();
+        foreach ($GroupPermission as $keyGroup => $allGroup) {
+        $allPermission = DB::table("permissions")->where('group','=',$allGroup->group)->get();
+            
+            $modal .=   '<tr>';
+            $modal .=   '<td style="border-style : hidden!important; vertical-align:middle;"><h5><b>'.strtoupper($allGroup->group).' :</b></h5></td>';
+                    foreach ($allPermission as $key => $value) {
+                    $CekPunya = DB::table('role_has_permissions')->where([['role_id','=',$rolesid],['permission_id','=',$value->id]])->count();
+                    $resPermission = explode(" ",$value->name);
+                        $modal .=   '<td scope="row" style="border-style : hidden!important;" ><fieldset>
+                                            <div class="checkbox checkbox-info checkbox-glow" >
+                                                <input type="checkbox" value="'.$value->id.'" name="permission_id[]" id="checkboxGlow1'.$value->id.'" 
+                                                '.((0 < $CekPunya)?'checked':"").'
+                                                >
+                                                <label for="checkboxGlow1'.$value->id.'" style="cursor: pointer;">'.$resPermission[0].'</label>
+                                            </div>
+                                        </fieldset>
+                                    </td>';
+                    }
+            $modal .=   '</tr>';
+        }
+            $modal .= '<input type="hidden" name="roleid" value="'.$rolesid.'" required></input>
+                    </tbody>
+                    </table>
+                  </div>
+                </div>';
+         return $modal;
+    }
+
 }
