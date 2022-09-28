c1d5430eb0e0b6a74bb874e594917e951fffe74f
suharyadi <suharyadi21@outlook.com>
2022-09-28 21:07:02 +0700

    - improve pdf result

--- app/Http/Controllers/Transaction/ReportResult.php
+++ app/Http/Controllers/Transaction/ReportResult.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Storage;
 use PDF;
 
+use App\Helpers\Helper as HelperTglIndo;
+
+use Carbon\Carbon;
+
 class ReportResult extends Controller
 {
     public function __construct(){
@@ -20,10 +24,18 @@ class ReportResult extends Controller
 
     public function ReportPDFResult($id_registration){
         $dec_penid = Crypt::decryptString($id_registration);//decrypt id
+
+        $resbasic = $this->GetInfoRegistration($dec_penid);//basic data
+        $age = Carbon::parse($resbasic->pastgllahir)->age;//to age
+        
         $data = [
             'title' => 'Tes untuk configurasi dompdf',
             'date' => date('m/d/Y'),
-            'id_registration' => $dec_penid
+            'id_registration' => $dec_penid,
+            'data_basic' => $resbasic,
+            'age' => $age,
+            'jeniskelamin' => $this->artijk($resbasic->pasjk),
+            'tanggalrujukindo' => HelperTglIndo::tanggal_indo($resbasic->pentglrujukan),
         ];
           
         $pdf = PDF::loadView('pages/report/result_report', $data);
@@ -31,4 +43,48 @@ class ReportResult extends Controller
         // return $pdf->download('Testing.pdf'); //ini untuk langsong download
         return $pdf->stream("Testing.pdf", array("Attachment" => false));// dipakai untuk tengok di browser
     }
+
+    //arti jenis kelamin
+    protected function artijk($jk){
+        if ($jk == "Man" || $jk == "man") {
+            return "Laki-laki";
+        }else if($jk == "Women" || $jk == "women"){
+            return "Perempuan";
+        }else{
+            return "Lainnya";
+        }
+    }
+
+    //cek akses
+    protected function CheckAcc(){
+        $userAcc = User::with('roles')->where('id','=', auth()->user()->id)->first();//get role user
+        return $userAcc;
+    }
+
+    protected function GetInfoRegistration($id_pen){
+        $data = DB::table('pendaftaran')
+            ->join('pasien','pasien.pasid','=','pendaftaran.penpasid')
+            ->join('pengirim','pengirim.pengid','=','pendaftaran.penpengid')
+            ->join('jenispembayaran','jenispembayaran.pemid','=','pendaftaran.penpemid')
+            ->orderBy('penid', 'desc')
+            ->where('pendaftaran.penid','=',$id_pen)
+            ->first();
+        return $data;
+    }
+
+    protected function GetInfoLaboratorium($id_pen){
+        $data = DB::table('pendaftaran')
+            ->join('pasien','pasien.pasid','=','pendaftaran.penpasid')
+            ->join('pengirim','pengirim.pengid','=','pendaftaran.penpengid')
+            ->join('jenispembayaran','jenispembayaran.pemid','=','pendaftaran.penpemid')
+            ->join('tindakankeluar','tindakankeluar.tndklrpenid','=','pendaftaran.penid')
+            ->join('tindakan','tindakan.tndid','=','tindakankeluar.tndklrtndid')
+            ->join('kategoritindakan','kategoritindakan.kattndid','=','tindakan.tndkattndid')
+            ->join('katlab', 'katlab.katlabtndid','=','tindakankeluar.tndklrtndid')
+            ->orderBy('penid', 'desc')
+            ->where('pendaftaran.penid','=',$id_pen)
+            ->get();
+        return $data;
+    }
+
 }
--- resources/views/pages/report/result_report.blade.php
+++ resources/views/pages/report/result_report.blade.php
@@ -1,11 +1,104 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Osmaro</title>
-</head>
+  <link href="http://fonts.cdnfonts.com/css/times-new-roman" rel="stylesheet">
+  <style>
+    @page { margin: 180px 50px; }
+    #header { position: fixed; left:-45px; top: -170px; right: -40px; height: 120px; text-align: center;}
+    #footer { position: fixed; left: -30px; bottom: -240px; right: -30px; height: 120px; }
+    #footer .page:after { content: counter(page, upper-roman);}
+  </style>
+  <title>Osmaro</title>
 <body>
-    <h1>{{ $title }}</h1>
-    <p>{{ $date }}</p>
-    <p>Testing untuk id registration <br>{{ $id_registration }}.</p>
+  <div id="header">
+    <img src="{{ public_path('images/logo_report/header.PNG') }}" style="width:100%;">
+  </div>
+  <div id="footer">
+    <img src="{{ public_path('images/logo_report/footer.PNG') }}" style="width:100%;">
+    {{-- isi content footer --}}
+  </div>
+  <div id="content">
+    <div style="margin-top: -30px; margin-left: 5px;">
+        
+        <table border="0">
+            <tr>
+                <td style="width: 320px; vertical-align: top;">
+                    <table border="0">
+                        <tr>
+                            <td style="padding: 5px;">Nama Pasien</td>
+                            <td style="padding: 5px; width: 1px;">:</td>
+                            <td style="padding: 5px;">{{ ucwords(strtolower($data_basic->pasnama)) }}</td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Umur</td>
+                            <td style="padding: 5px; width: 1px;">:</td>
+                            <td style="padding: 5px;">{{ $age }} Tahun</td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Jenis Kelamin</td>
+                            <td style="padding: 5px; width: 1px;">:</td>
+                            <td style="padding: 5px;">{{ ucwords(strtolower($jeniskelamin)) }}</td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Alamat</td>
+                            <td style="padding: 5px; width: 1px;">:</td>
+                            <td style="padding: 5px;">{{ ucwords(strtolower($data_basic->pasalamat)) }}</td>
+                        </tr>
+                    </table>
+                </td>
+                <td style="width:20px;">
+                    
+                </td>
+                <td style="vertical-align: top; width: 270px;">
+                    <table border="0">
+                        <tr>
+                            <td style="padding: 5px;">Kode Lab</td>
+                            <td style="padding: 5px; width: 1px;">:</td>
+                            <td style="padding: 5px;">2209030001</td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Tanggal</td>
+                            <td style="padding: 5px; width: 1px;">:</td>
+                            <td style="padding: 5px;">{{ $tanggalrujukindo }}</td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Pengirim</td>
+                            <td style="padding: 5px; width: 1px;">:</td>
+                            <td style="padding: 5px;">Value</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+        </table>
+    </div>
+    <br>
+    <div>
+        <center>
+            <p><h2>HASIL PEMERIKSAAN LABORATORIUM</h2></p>
+            <table class="tableee">
+                <tr class="tableee">
+                    <th class="tableee">Pemeriksaan</th>
+                    <th class="tableee">Hasil</th>
+                    <th class="tableee">Satuan</th>
+                    <th class="tableee">Nilai Normal</th>
+                </tr>
+            </table>
+        </center>
+    </div>
+    <p style="page-break-before: always; margin-top: -40px;">the second page</p>
+  </div>
 </body>
 </html>
+<style type="text/css">
+    .tableee {
+      width: 105px;
+      border: 1px solid black;
+      border-collapse: collapse;
+      margin-left: -20px;
+      padding-top: 10px;
+      padding-left: 30px;
+      padding-right: 30px;
+
+    }
+</style>
